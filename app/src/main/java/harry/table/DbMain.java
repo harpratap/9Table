f65f99364316cd63c30afe8578cf71abd537f6ed
harpratap <harryhades@ymail.com>
2015-02-13 00:56:49 +0530

    Included premade sql lite databases and listview to read it.

---
+++ app/src/main/java/harry/table/DbMain.java
@@ -0,0 +1,32 @@
+package harry.table;
+
+/**
+ * Created by Harpratap on 13-02-2015.
+ */
+import android.app.ListActivity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.SimpleCursorAdapter;
+public class DbMain extends ListActivity {
+    private Cursor employees;
+    private MyDatabase db;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        db = new MyDatabase(this);
+        employees = db.getEmployees(); // you would not typically call this on the main thread
+        ListAdapter adapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_1,
+                employees,
+                new String[] {"FirstName"},
+                new int[] {android.R.id.text1});
+        getListView().setAdapter(adapter);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        employees.close();
+        db.close();
+    }
+}
